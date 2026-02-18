f1236789ca88a829694992ae2c6dee28f2f59bc0
Nicolas de Moura Tomaz da Silva <nicolasmoura632@gmail.com>
2026-02-18 19:21:18 -0300

    Update USAclock.java

--- EXERCÍCIOS/[4 - Herança e Polimorfismo em Java]/4.3 (Relógios)/USAclock.java
+++ EXERCÍCIOS/[4 - Herança e Polimorfismo em Java]/4.3 (Relógios)/USAclock.java
@@ -1,3 +1,48 @@
-public class USAclock extends Clock{
+public non-sealed class USAclock extends Clock{
+    private String periodIndicator;
+
+    public String getPeriodIndicator() {
+        return periodIndicator;
+    }
+
+    public void setAfterMidDay(){
+        this.periodIndicator =  "PM";
+    }
+
+    public void setBeforeMidDay(){
+        this.periodIndicator = "AM";
+    }
+
+
+    public void setHoras(int horas) {
+        setBeforeMidDay();
+        if (horas > 12 && (horas <= 23)){
+            setAfterMidDay();
+            this.horas = horas - 12;
+            return;
+        }else if (horas >= 24){
+            this.horas = 0;
+        } else 
+            this.horas = horas;
+    }
+
+    @Override
+    public Clock convert(Clock clock) {
+        this.segundos = clock.getSegundos();
+        this.minutos = clock.getMinutos();
+        switch (clock){
+            case USAclock usaClock ->{
+                this.horas = usaClock.getHoras();
+                this.periodIndicator = usaClock.getPeriodIndicator();
+            }    
+            case BRClock brClock -> this.setHoras(brClock.getHoras());
+        }
+        return this;
+    }
+     
+    @Override
+    public String getTime(){
+        return super.getTime() + " " + this.periodIndicator;
+    }
 
 }
