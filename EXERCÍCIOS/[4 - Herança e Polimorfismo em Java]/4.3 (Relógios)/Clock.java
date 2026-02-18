88f9ee6b3d48dbeace039792a14589f5d044dcd5
Nicolas de Moura Tomaz da Silva <nicolasmoura632@gmail.com>
2026-02-18 19:20:52 -0300

    Clock.java

--- EXERCÍCIOS/[4 - Herança e Polimorfismo em Java]/4.3 (Relógios)/Clock.java
+++ EXERCÍCIOS/[4 - Herança e Polimorfismo em Java]/4.3 (Relógios)/Clock.java
@@ -1,24 +1,42 @@
-public class Clock {
-    private int horas;
-    private int minutos;
-    private int segundos;
+public sealed abstract class Clock permits BRClock, USAclock{
+    protected int horas;
+    protected int minutos;
+    protected int segundos;
 
     public int getHoras() {
         return horas;
     }
     public void setHoras(int horas) {
+        if (horas > 24){
+            this.horas = 24;
+            return;
+        }
         this.horas = horas;
     }
     public int getMinutos() {
         return minutos;
     }
     public void setMinutos(int minutos) {
+        if (minutos >= 60){
+            this.minutos = 60;
+            return;
+        }
         this.minutos = minutos;
     }
     public int getSegundos() {
         return segundos;
     }
     public void setSegundos(int segundos) {
+        if (segundos >= 60){
+            this.segundos = 60;
+            return;
+        }
         this.segundos = segundos;
     }
+    
+    public String getTime(){
+        return horas + ":" + minutos + ":" + segundos;
+    }
+
+    abstract Clock convert(Clock clock);
 }
